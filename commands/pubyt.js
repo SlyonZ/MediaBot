a3f5cc12ca6334875057db7d903b0e3b315149e7
SlyonZ <39380843+SlyonZ@users.noreply.github.com>
2018-06-13 10:15:23 +0200

    Add files via upload

---
+++ commands/pubyt.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+exports.run = (client, message, args) => {
+
+
+
+  let roleAnnonces = message.guild.roles.find("name", "annonces");
+
+  const embed = new Discord.RichEmbed()
+  .setAuthor("Un nouveau Best Of Sardoche est sorti !", "https://imgur.com/Pem0gkd.png")
+
+  .setColor(0xFF0000)
+  .setFooter("Bon visionnage ! ", "https://imgur.com/rDyK8BU.png")
+  .setImage("https://i.imgur.com/DkXCftA.png")
+
+  .setTimestamp()
+
+  .addField("Lien de la vidéo :", "https://www.youtube.com/watch?v=anmggl_CplA", true);
+
+  message.delete()
+  message.channel.send({embed});
+  message.channel.send("<@&451258312575549440>");
+  }
+
+
+    exports.conf = {
+      enabled: true,
+      guildOnly: false,
+      aliases: [],
+      permLevel: 2
+    };
+
+    exports.help = {
+      name: 'pubyt',
+      description: 'Lance une annonce quand un nouveau bos sort',
+      usage: 'pubyt'
+    };

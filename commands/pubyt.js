23b2687c7b3da4615408bdc78e4902bf99ad8619
SlyonZ <39380843+SlyonZ@users.noreply.github.com>
2018-06-17 15:39:18 +0200

    Update pubyt.js

--- commands/pubyt.js
+++ commands/pubyt.js
@@ -17,7 +17,7 @@ exports.run = (client, message, args) => {
 
   message.delete()
   message.channel.send({embed});
-  message.channel.send("<@&451258312575549440>");
+  message.channel.send("@everyone");
   }
 
 
